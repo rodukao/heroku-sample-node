8a481f0013e9b82cf469eb842815d868d3c6226e
rodukao <rodukao@gmail.com>
2022-08-24 10:27:50 -0300

    implement local database connection

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+var mysql = require('mysql');
+const db_connection = require('./database-connection')
+const connection = mysql.createConnection(db_connection)
+
+//EXPRESS
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+//connect to database
+connection.connect();
+connection.query('SELECT * FROM usuarios', function (error, results, fields) {
+  if (error) throw error;
+  app.get('/', (req, res) => {
+    res.send(results[0])
+  })
+});
+connection.end();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
