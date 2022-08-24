8dcdf9d812925bc97cf721f52df687f848e5d302
rodukao <rodukao@gmail.com>
2022-08-24 16:03:14 -0300

    using express static to display html pages

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-var mysql = require('mysql');
+var mysql = require('mysql')
+require('dotenv').config()
 
 var connection = mysql.createConnection({
   host     : process.env.host,
   user     : process.env.user,
   password : process.env.password,
   database : process.env.database
-});
+})
 
 //EXPRESS
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static('public'));
+
 
 //connect to database
-connection.connect();
+connection.connect()
 connection.query('SELECT * FROM usuarios', function (error, results, fields) {
-  if (error) throw error;
+  if (error) throw error
     app.get('/', (req, res) => {
     res.send(results)
   })
-});
-connection.end();
+})
+connection.end()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
