aba004d9f3193edf5b8594023d65c46b5304ef30
Rodukao <rodukao@gmail.com>
2022-08-28 19:24:10 -0300

    adicionei página de Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,9 +25,38 @@ exports.register = (req, res) => {
             })
         }
 
-        let senhaCriptografada = await bcrypt.hash(senha, 8)
-        console.log(senhaCriptografada)
-        
+        else{
+            
+            let senhaCriptografada = await bcrypt.hash(senha, 8)
+            connection.query(`INSERT INTO usuarios (nome, email, senha, meta) values ('${nome}', '${email}', '${senhaCriptografada}', '${objetivo}')`, (error, result) => {
+                if(error) {
+                    throw error
+                }
+                console.log(result)
+                })
+                connection.end()
+                return res.render('./', {
+                    message : "Usuário cadastrado com sucesso."
+            }) 
+        }   
     })
-    
+}
+
+exports.login = (req, res) => {
+    const { email, senha } = req.body
+    connection.query(`SELECT email, senha FROM usuarios WHERE email = '${email}'`, (error, result) => {
+        if(error) throw error
+        
+        if(result.length == 0) {
+            return res.render('login', {
+                message: "Email não encontrado"
+            })
+        } else {
+            console.log(`${result[0]}`)
+            connection.end()
+            return res.render('register', {
+                message: "LALALALA"
+            })
+        }
+    }) 
 }
